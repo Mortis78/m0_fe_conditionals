42d4490e7ee86d245e249e7a0ee11cd379311093
Mortis78 <113194002+Mortis78@users.noreply.github.com>
2022-09-21 18:48:45 -0600

    Update comparisons.js

--- comparisons.js
+++ comparisons.js
@@ -22,25 +22,26 @@ console.log("Is numberTeachers greater than numberStudents?", numberTeachers > n
 
 // YOU DO: log the result of the comparison: is numberTeachers less than numberStudents?
 // this should log: true
+console.log("is numberTeachers less than numberStudents?", numberTeachers < numberStudents );
 
 // YOU DO: log the result of the comparison: is numberTeachers strictly equal to stringTeachers?
 // this should log: false
-
+console.log("is numberTeachers strictly equal to stringTeachers?", numberTeachers === stringTeachers);
 // YOU DO: log the result of the comparison: is numberTeachers not equal to numberStudents?
 // this should log: true
-
+console.log("is numberTeachers not equal to numberStudents?", numberTeachers !== numberStudents);
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 20?
-// this should log: true
-
+// this should log: true 
+console.log("is numberStudents greater than or equal to 20?", numberStudents >= 20);
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 21?
 // this should log: false
-
+console.log("is numberStudents greater than or equal to 21?", numberStudents >= 21);
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 20?
 // this should log: true
-
+console.log("is numberStudents less than or equal to 20?", numberStudents <= 20);
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 21?
 // this should log: true
-
+console.log("is numberStudents less than or equal to 21?", numberStudents <= 21);
 
 // #-------------------
 // PART 2: Articulating what you are doing
@@ -52,21 +53,28 @@ console.log("Is numberTeachers greater than numberStudents?", numberTeachers > n
 // Make sure YOU can explain it that way!
 
 console.log(4 < 9);
-// YOU DO: Explain.
+// YOU DO: consol.log is what will give us the output.
+//the input is a mathmaticol expression asking the question; is the intager 4 less than the intager 9. 
 
 var books = 3;
 console.log(4 < books);
-// YOU DO: Explain.
+// YOU DO: book is a variable that has the assigned intager value of 3.
+// console.log will give us an output after evaluating ( is 4 less than the value assinged to the variable books{3}) 
+// this in going to come back as ither true of false. in this case it would be false
 
 var friends = 6;
 var siblings = 2;
 console.log(friends > siblings);
-// YOU DO: Explain.
+// YOU DO:we have two variables, friends and siblings. both have assinged values.
+// its asking through the use of these variables if 6 is greater than 2
+// console.log will give us the output True.
+
 
 var attendees = 9;
 var meals = 8;
 console.log(attendees != meals);
-// YOU DO: Explain.
+// YOU DO: Explain. again we have two variables; attendees, and meals.
+// this line is asking (if 9 isNot equel to 8 ) this would come back as True, 9 isNot equel to 8.
 
 
 // #-------------------
@@ -87,17 +95,21 @@ var lovesDogPark = false;
 var lovesTreats = true;
 var age = 1;
 
+
 // YOU DO:
 // Determine if the dog loves to play and loves treats
-
+consol.log(lovesToPlay && lovesTreats);
 
 // Determine if the dog loves to play and loves the dog park
-
+consol.log(lovesToPlay && lovesDogPark );
 
 // Determine if the dog loves to play or loves the dog park
-
+consol.log(lovesToPlay || lovesDogPark );
 
 // Determine if the dog loves to play and is a puppy
-
+consol.log(lovesToPlay && age );
 // What did your final line of code evaluate to? Why do you think that is? Explain.
-// ANSWER:
+// ANSWER: it returend 1. for boolion 1 / 0 con equel true or false.
+//i think for this problem the bool evaluated age as true, so it returned 1.
+// i supose it could have also not known the answer so it added the two options (1 or 0) 
+//as intagers (1 + 0 = 1 ) and returned that.. 
